0bb54d152c0fef231bbc0d01120ecab2dc0b6076
Chamba Nanang <123768840+cypha-io@users.noreply.github.com>
2025-03-04 22:16:19 +0000

    Update UserDashboard.js

--- src/pages/UserDashboard.js
+++ src/pages/UserDashboard.js
@@ -8,7 +8,7 @@ function UserDashboard() {
     const [data, setData] = useState([
         { name: 'Jan', investment: 0, balance: 0 },
         { name: 'Feb', investment: 0, balance: 0 },
-        { name: 'Mar', investment: 994, balance: 1054 },
+        { name: 'Mar', investment: 994.00, balance: 1054.00 },
         { name: 'Apr', investment: 0, balance: 0 },
         { name: 'May', investment: 0, balance: 0 },
     ]);
